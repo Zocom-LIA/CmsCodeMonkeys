3af3f15a89f4e48cbd8229ce17978e4ef5aa640d
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-08-12 12:16:29 +0200

    Initial commit

---
+++ CodeMonkeys.CMS.Public/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using CodeMonkeys.CMS.Public.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace CodeMonkeys.CMS.Public.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
