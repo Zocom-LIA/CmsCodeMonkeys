e47d4ab27bd5e5ec4cab472c5cdf0ab673693d07
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-08-13 09:24:08 +0200

    Initial Commit

---
+++ CodeMonkeys.CMS.Public.Shared/Repository/IEntity.cs
@@ -0,0 +1,42 @@
+﻿//
+// Class: RepositoryBase
+//
+// Description:
+// The RepositoryBase class is a base class for repositories that interact with a database context. It provides a set of methods for querying, adding, updating, and deleting entities in the database, as well as handling exceptions that occur during data access operations. The class is designed to be generic and flexible, allowing it to work with any entity type.
+//
+// Purpose:
+// The purpose of the RepositoryBase class is to provide a common set of methods for working with a database context in a consistent and reusable way. By defining a standard set of operations for managing entities, developers can write code that is more modular, flexible, and maintainable, leading to higher-quality software. The class also helps to decouple the data access logic from the rest of the application, making it easier to test and refactor the code in the future.
+//
+// Usage:
+// The RepositoryBase class is typically used as a base class for concrete repository implementations that interact with a specific database context. Developers can create concrete repository classes that inherit from the RepositoryBase class, providing a consistent and reusable way to interact with the database. By using the repository pattern, developers can write code that is more modular, flexible, and maintainable, leading to a more robust and scalable application.
+//
+// MIT License
+//
+// Copyright (c) 2024 Ulf Bourelius
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+namespace CodeMonkeys.CMS.Public.Shared.Repository
+{
+    public interface IEntity
+    {
+        object GetIdentifier();
+    }
+}
---
+++ CodeMonkeys.CMS.Public.Shared/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using CodeMonkeys.CMS.Public.Shared.Repository;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace CodeMonkeys.CMS.Public.Shared.Repository
+{
+    public interface IRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        Task<TEntity?> AddAsync(TEntity entity, CancellationToken cancellation = default);
+        Task<int> AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellation = default);
+        Task<int> CountAsync(CancellationToken cancellation = default);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellation = default);
+        Task<IEnumerable<TEntity>> GetAllAsync(int pageIndex = 0, int pageSize = 10, CancellationToken cancellation = default);
+        Task<TEntity?> GetByIdAsync(TIdentifier id, CancellationToken cancellation = default);
+        Task<int> RemoveAsync(TEntity entity, CancellationToken cancellation = default);
+        Task<int> RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellation = default);
+        Task<TEntity?> UpdateAsync(TEntity entity, CancellationToken cancellation = default);
+        Task<IEnumerable<TEntity>> UpdateRangeAsync(TEntity entity, CancellationToken cancellation = default);
+    }
+}
