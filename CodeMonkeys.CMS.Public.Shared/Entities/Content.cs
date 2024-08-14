9e7b349caf79fd98411a77d9d9d433f7aa10f410
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-08-14 10:16:19 +0200

    Update Content.cs Contenttype

--- CodeMonkeys.CMS.Public.Shared/Entities/Content.cs
+++ CodeMonkeys.CMS.Public.Shared/Entities/Content.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         required public string Title { get; set; }
+        required public string ContentType { get; set; }
+        required public string Body { get; set; }
         required public DateTime CreatedDate { get; set; }
         required public DateTime LastModifiedDate { get; set; }
 
