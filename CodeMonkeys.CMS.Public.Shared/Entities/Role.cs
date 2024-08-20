2e2e17aa0db30dd18ae47cb4dc9a147c04d2bd99
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-08-20 11:48:23 +0200

    Added selected Role to User on Registration

--- CodeMonkeys.CMS.Public.Shared/Entities/Role.cs
+++ CodeMonkeys.CMS.Public.Shared/Entities/Role.cs
@@ -4,11 +4,6 @@ namespace CodeMonkeys.CMS.Public.Shared.Entities
 {
     public class Role : IdentityRole<Guid>
     {
-        // IdentityRole<Guid> already includes Id (of type Guid) and Name properties
-        // You don't need to redefine RoleID or RoleName
-
-        // Enum to define different roles
-        public UserRole UserRoles { get; set; }
     }
 
 
---
+++ CodeMonkeys.CMS.Public.Shared/Migrations/20240820093927_init3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeMonkeys.CMS.Public.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class init3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserRoles",
+                table: "AspNetRoles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserRoles",
+                table: "AspNetRoles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
