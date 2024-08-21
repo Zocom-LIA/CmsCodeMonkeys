21365913b47a09867b03f9e16a410677ceb95054
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-08-21 10:16:51 +0200

    Update User.cs
    UserRole should not be part of User since it has its own table scaffolded by IdentityUser and IdentityRole
    
    Signed-off-by: Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>

--- CodeMonkeys.CMS.Public.Shared/Entities/User.cs
+++ CodeMonkeys.CMS.Public.Shared/Entities/User.cs
@@ -5,7 +5,5 @@ namespace CodeMonkeys.CMS.Public.Shared.Entities
     public class User : IdentityUser<Guid>
     {
         public ICollection<WebPage> Pages { get; set; } = new List<WebPage>();
-
-        public UserRole UserRoles { get; set; }
     }
 }
