278ce96e62f1f495e2b4c86888adf586c8820b9b
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-08-30 14:11:18 +0200

    Modified the UI for content type in content. Will not be take values from an enum.

---
+++ CodeMonkeys.CMS.Public.Shared/Entities/ContentTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CodeMonkeys.CMS.Public.Shared.Entities
+{
+    public enum ContentTypes
+    {
+        Text,
+        Image,
+        Video
+    }
+}
