2bcc42688d67cc9fc7efc8b034c1554b4d10d38b
FelixEdenborgh <Felix@edenborgh.se>
2024-09-18 10:56:33 +0200

    update

--- CodeMonkeys.CMS.Public.Shared/Entities/Content.cs
+++ CodeMonkeys.CMS.Public.Shared/Entities/Content.cs
@@ -22,7 +22,6 @@ namespace CodeMonkeys.CMS.Public.Shared.Entities
         public int? WebPageId { get; set; }
         public WebPage? WebPage { get; set; }
 
-        public string Color { get; set; } = "#1e1e1e";
 
         public object GetIdentifier() => ContentId;
     }
