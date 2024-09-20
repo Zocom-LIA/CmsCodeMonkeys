9e0312479524278c6ad6d96b916b976938bd5640
FelixEdenborgh <Felix@edenborgh.se>
2024-09-20 14:57:54 +0200

    mhe

---
+++ CodeMonkeys.CMS.Public.Shared/Entities/GridElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMonkeys.CMS.Public.Shared.Entities
+{
+    public class GridElement
+    {
+        public string? Type { get; set; }
+        public string? Body { get; set; }
+        public string? Color { get; set; }
+        public string? FontSize { get; set; }
+        public string? FontFamily { get; set; }
+    }
+}
