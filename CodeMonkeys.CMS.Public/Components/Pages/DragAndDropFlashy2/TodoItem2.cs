e9e5f3cd692b76f2b05453930a756b24749cf89d
nraxi <olle.code@hotmail.com>
2024-09-26 23:27:48 +0200

    Add Link logic

--- CodeMonkeys.CMS.Public/Components/Pages/DragAndDropFlashy2/TodoItem2.cs
+++ CodeMonkeys.CMS.Public/Components/Pages/DragAndDropFlashy2/TodoItem2.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace CodeMonkeys.CMS.Public.Components.Pages.DragAndDropFlashy2
 {
@@ -10,9 +9,28 @@ namespace CodeMonkeys.CMS.Public.Components.Pages.DragAndDropFlashy2
         public bool ShowEditButton { get; set; } = true;
         public int FontSize { get; set; } = 16; // Default font size
         public string TextColor { get; set; } = "Black"; // Default text color
-        public string? Color { get; set; } 
+        public string? Color { get; set; }
         public bool IsBold { get; set; } // New property for bold
         public bool IsItalic { get; set; } // New property for italic
         public string FontFamily { get; set; } = "Arial"; // Default font family
+
+        private bool isLinkEnabled; // Backing field for IsLinkEnabled
+        public bool IsLinkEnabled
+        {
+            get => isLinkEnabled;
+            set
+            {
+                isLinkEnabled = value;
+                if (!value)
+                {
+                    // Clear the link information when the link is disabled
+                    LinkUrl = string.Empty;
+                    LinkDescription = string.Empty;
+                }
+            }
+        }
+
+        public string? LinkUrl { get; set; } // For the link URL
+        public string? LinkDescription { get; set; } = string.Empty; // For the link description
     }
 }
