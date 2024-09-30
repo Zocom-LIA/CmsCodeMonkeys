2117e389c28d8e43ee493cefa0755a91bc1b5bcd
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-09-30 12:00:29 +0200

    Added Db Configuration files

---
+++ CodeMonkeys.CMS.Public.Shared/Configuration/ContentConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using CodeMonkeys.CMS.Public.Shared.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CodeMonkeys.CMS.Public.Shared.Configuration
+{
+    public class ContentConfiguration : IEntityTypeConfiguration<Content>
+    {
+        public void Configure(EntityTypeBuilder<Content> builder)
+        {
+            builder.ToTable("Contents");
+
+            builder.HasKey(content => content.ContentId);
+
+            builder.Property(content => content.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(site => site.CreatedDate)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(site => site.LastModifiedDate)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(content => content.OrdinalNumber)
+                .IsRequired();
+
+            builder.Property(content => content.Color)
+                .IsRequired()
+                .HasMaxLength(7)
+                .HasDefaultValue("#1e1e1e");
+
+            builder.HasOne(content => content.Author)
+                .WithMany()
+                .HasForeignKey(content => content.AuthorId);
+
+            builder.HasOne(content => content.WebPage)
+                .WithMany()
+                .HasForeignKey(content => content.WebPageId);
+
+            builder.HasDiscriminator<string>("ContentType")
+                .HasValue<TextContent>(nameof(TextContent))
+                .HasValue<ImageContent>(nameof(ImageContent))
+                .HasValue<VideoContent>(nameof(VideoContent))
+                .HasValue<LinkContent>(nameof(LinkContent))
+                .HasValue<CodeContent>(nameof(CodeContent))
+                .HasValue<FileContent>(nameof(FileContent))
+                .HasValue<CodeContent>(nameof(CodeContent))
+                .HasValue<QuoteContent>(nameof(QuoteContent));
+        }
+    }
+}
---
+++ CodeMonkeys.CMS.Public.Shared/Configuration/SectionConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using CodeMonkeys.CMS.Public.Shared.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CodeMonkeys.CMS.Public.Shared.Configuration
+{
+    public class SectionConfiguration : IEntityTypeConfiguration<Section>
+    {
+        public void Configure(EntityTypeBuilder<Section> builder)
+        {
+            builder.Property(section => section.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(section => section.Description)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(section => section.OrdinalNumber)
+                .IsRequired();
+
+            builder.Property(section => section.IsVisible)
+                .IsRequired();
+
+            builder.HasOne(section => section.WebPage)
+                .WithMany(page => page.Sections)
+                .HasForeignKey(section => section.WebPageId);
+
+        }
+    }
+
+
+}
