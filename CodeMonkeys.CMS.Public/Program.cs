25cf7655e72089cf1f6a9609f72a92c6c513ce9d
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-10-01 15:00:14 +0200

    Removed dead code
    If we reintroduce this, we need to think of how to make it
    compatible with the new.
    Safer not to have it too easily available.

--- CodeMonkeys.CMS.Public/Program.cs
+++ CodeMonkeys.CMS.Public/Program.cs
@@ -5,7 +5,6 @@ using CodeMonkeys.CMS.Public.Components.Account;
 using CodeMonkeys.CMS.Public.Shared;
 using CodeMonkeys.CMS.Public.Shared.Data;
 using CodeMonkeys.CMS.Public.Shared.Entities;
-using CodeMonkeys.CMS.Public.Shared.Middleware;
 using CodeMonkeys.CMS.Public.Shared.Profiles;
 using CodeMonkeys.CMS.Public.Shared.Repository;
 using CodeMonkeys.CMS.Public.Shared.Services;
@@ -73,7 +72,6 @@ builder.Services.AddScoped<IPageStatsRepository, PageStatsRepository>();
 builder.Services.AddScoped<IPageStatsService, PageStatsService>();
 builder.Services.AddScoped<IMenuRepository, MenuRepository>();
 builder.Services.AddScoped<IMenuService, MenuService>();
-builder.Services.AddScoped<VisitCounterMiddleware>();
 builder.Services.AddScoped<ISiteRepository, SiteRepository>();
 builder.Services.AddScoped<IWebPageRepository, WebPageRepository>();
 builder.Services.AddScoped<IContentRepository, ContentRepository>();
@@ -107,7 +105,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.UseAntiforgery();
 
-//app.UseMiddleware<VisitCounterMiddleware>();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
