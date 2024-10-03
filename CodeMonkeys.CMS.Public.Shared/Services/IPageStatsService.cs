f9266fa97d98e94745ad7d7ae14cf30241b35a0f
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-10-03 13:32:10 +0200

    Filter page visits page on site id

--- CodeMonkeys.CMS.Public.Shared/Services/IPageStatsService.cs
+++ CodeMonkeys.CMS.Public.Shared/Services/IPageStatsService.cs
@@ -6,7 +6,6 @@ namespace CodeMonkeys.CMS.Public.Shared.Services
     public interface IPageStatsService
     {
         Task<int> GetPageVisitsAsync(string pageUrl);
-        Task<IEnumerable<PageStats>> GetPageStatsAsync();
         Task<IEnumerable<PageStats>> GetPageStatsAsync(int siteId);
     }
 }
