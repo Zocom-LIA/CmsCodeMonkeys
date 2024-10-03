b93e8d6f41fa7420302c04d0789524afe3226b7c
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-10-03 14:31:33 +0200

    New component for rendering content to visitors at "/content/{webPageId}"

---
+++ CodeMonkeys.CMS.Public/Components/Pages/SiteContentViewer.razor.cs
@@ -0,0 +1,59 @@
+﻿using CodeMonkeys.CMS.Public.Components.Shared;
+using CodeMonkeys.CMS.Public.Shared.Entities;
+using CodeMonkeys.CMS.Public.Shared.Services;
+
+using Microsoft.AspNetCore.Components;
+
+namespace CodeMonkeys.CMS.Public.Components.Pages
+{
+    public partial class SiteContentViewer : ContentBaseComponent<SiteContentViewer>
+    {
+        [Parameter] public int WebPageId { get; set; }
+        public WebPage? WebPage { get; set; }
+
+        [Inject] required public ISiteService SiteService { get; set; }
+        [Inject] required public ISectionService SectionService { get; set; }
+
+        private Dictionary<int, Section> Sections { get; set; } = new Dictionary<int, Section>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ExecuteWithLoadingAsync(async () =>
+            {
+                await base.OnInitializedAsync();
+
+                WebPage ??= await WebPageService.GetVisitorWebPageAsync(WebPageId, includeSections: true, includeContents: true);
+
+                if (WebPage == null)
+                {
+                    Navigation.NavigateTo("/error");
+                }
+            });
+        }
+
+        private WebPage NoLandingPageConfiguredPage()
+        {
+            // Generate an error message that explains that the site is not yet published. 
+            return new WebPage() { Title = "The site is not yet published", Contents = [new Content() { Body = "The site is not yet published. Please try again later." }] };
+        }
+
+        private WebPage NoSuchPagePage(string PageName)
+        {
+            return new WebPage() { Title = "No such page", Contents = [new Content() { Body = $"No such page: {PageName}" }] };
+        }
+
+        private RenderFragment RenderSection(Section section) => builder =>
+        {
+            builder.OpenElement(0, "div");
+            builder.AddAttribute(1, "class", $"section-{section.SectionId}"); // Add a class to the div element with the section ID
+            builder.AddAttribute(2, "style", $"color: {section.Color}");
+
+            int seq = 3;
+            foreach (var content in section.ContentItems)
+            {
+                builder.AddContent(seq++, content.RenderContent());
+            }
+            builder.CloseElement();
+        };
+    }
+}
