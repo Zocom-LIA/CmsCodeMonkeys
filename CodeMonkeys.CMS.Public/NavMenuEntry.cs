e6d6fddc72cb63fa0c9c92b15c82a2b80cdd912c
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-10-10 16:38:15 +0200

    Construct nav menu from data store

---
+++ CodeMonkeys.CMS.Public/NavMenuEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeMonkeys.CMS.Public
+{
+    public class NavMenuEntry
+    {
+        public bool ShowWhenAuthorized { get; set; } = false;
+        public bool ShowWhenUnauthorized { get; set; }= false;
+        public string Href { get; set; }
+        public string LinkText { get; set; }
+        public string? EmptySpanClass { get; set; }
+    }
+}
