3b0929f46182137db5c019420801a55026126ad1
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-10-11 15:51:50 +0200

    Linked the Nav Menu to the Menu Configuration Service

--- CodeMonkeys.CMS.Public/Services/MenuConfigurationService.cs
+++ CodeMonkeys.CMS.Public/Services/MenuConfigurationService.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeMonkeys.CMS.Public.Services
+﻿using System.Collections.Immutable;
+
+namespace CodeMonkeys.CMS.Public.Services
 {
     public class MenuConfigurationService
     {
@@ -9,13 +11,37 @@
             set
             {
                 _isEnabled = value;
-                foreach (Action action in actions)
-                {
-                    action();
-                }
+                Notify();
 
             }
         }
-        public HashSet<Action> actions { get; set; } = new();
+
+        void Notify()
+        {
+            foreach (Action action in NotificationActions)
+            {
+                action();
+            }
+        }
+        public HashSet<Action> NotificationActions { get; } = new();
+
+        // We absolutely do not want to provide a reference to the list that could be used to change it without triggering the notifications.
+        private ImmutableList<NavMenuEntry> _entries = new List<NavMenuEntry>().ToImmutableList();
+        public ImmutableList<NavMenuEntry> NavMenuEntries { get { return _entries; } }
+
+        // Intent at time of coding is that the empty list above has priority 0, the default list from the NavMenu has priority 1,
+        // and lists provided by page components have priority 2.
+        private int currentPriority = 0;
+
+        // Full replacement only.
+        public void SetEntries(ImmutableList<NavMenuEntry> entries, int priority)
+        {
+            if (priority > currentPriority)
+            {
+                _entries = entries;
+                currentPriority = priority;
+                Notify();
+            }
+        }
     }
 }
